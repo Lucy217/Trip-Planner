26b6d0c9aba02c8f515d9cbbbe46c4bd81964851
hyeon9615 <juhyeon9615@gmail.com>
2019-05-20 11:14:44 +0900

    no message

--- app/src/main/java/com/sj/realtrippro/memo_detail.java
+++ app/src/main/java/com/sj/realtrippro/memo_detail.java
@@ -3,7 +3,7 @@ package com.sj.realtrippro;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class memo_detail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
